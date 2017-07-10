83386e4eb6b0a044ced569f6357bce32530ca2e3
Roman Blanyar <romanb@oss-usa.com>
2017-07-10 03:27:34 +0300

    first commit

--- server/server.js
+++ server/server.js
@@ -9,10 +9,21 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected.');
+
+  socket.emit('newMessage', {
+    from: 'ryb_lug@mail.ru',
+    text: 'Hey',
+    createdAt: 123123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected.');
